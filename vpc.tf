55bd3922b2a77bc338559b002621b0490628ee7c
Anoop G Nair <anoop@blog.mysite.com>
2021-05-18 10:53:33 +0530

    vpc creation

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,24 @@
-resource "aws_vpc" "webapp" {
-  cidr_block           = var.vpc.cidr
+# ===============================================================================
+# Gathering All Subent Name
+# ===============================================================================
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+# ===============================================================================
+# vpc creation
+# ===============================================================================
+resource "aws_vpc" "vpc" {
+    
+  cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
-
   tags = {
-    Name = var.vpc.name
+    Name = "${var.project}-vpc"
   }
+    
 }
+
